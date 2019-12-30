ae202dfc0014222b862374a95da804c8c4611194
yixiWang <1530711658@qq.com>
2019-12-30 09:00:49 +0800

    提交2

---
+++ day2/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+   <h1>
+   简单书写一个home页面
+   </h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'home',
+  // components: {
+    
+  // }
+}
+</script>
